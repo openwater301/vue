0400a0b7d4baeff97c0b7d9298b0da56beb5426a
openwater301 <openwater301@hanmail.net>
2023-07-12 06:14:40 +0900

    완성코드

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+
+  created() {
+	if (localStorage.length > 0) {
+		for (var i = 0; i < localStorage.length; i++) {
+			this.todoItems.push(localStorage.key(i));
+		}
+	 }
+  },
+
+  methods: {
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },    
+		addTodo(todoItem) {
+		    localStorage.setItem(todoItem, todoItem);
+		    this.todoItems.push(todoItem);
+		},
+  },
+
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style>
+  body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {border:1px solid red;
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in propsdata" class="shadow">
+        <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+          <i class="fa fa-trash-o" aria-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['propsdata'],  
+
+
+
+  methods: {
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+ul {
+  list-style:none;
+  padding-left:0;
+  margin-top:0;
+  text-align:left;
+}
+ul li {
+  display:flex;
+  min-height:50px; height:50px;
+  line-height:50px;
+  margin:0.5rem 0;
+  padding:0 0.9rem;
+  background-color:white;
+  border-radius:5px;
+}
+.checkBtn{
+  border:1px solid blue;
+  line-height:45px;
+  color:#62acde; 
+  margin-right:50px;
+}
+.removeBtn{
+  border:1px solid black;
+  margin-left:auto;
+  color:#de4343; 
+}
+</style>
